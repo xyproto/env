814a125c1a3eb07333c905f7d5bc0b65ecc6785b
Alexander F. Rødseth <rodseth@gmail.com>
2021-11-04 11:03:33 +0100

    Add tests

---
+++ env_test.go
@@ -0,0 +1,33 @@
+package env
+
+import (
+	"os"
+	"testing"
+)
+
+func TestContains(t *testing.T) {
+	os.Setenv("ASDF", "123asdf123")
+	if !Contains("ASDF", "asdf") {
+		t.Fail()
+	}
+}
+
+func TestHas(t *testing.T) {
+	if Has("ASDFASDFASDF") {
+		t.Fail()
+	}
+	os.Setenv("ASDFASDFASDF", "1")
+	if !Has("ASDFASDFASDF") {
+		t.Fail()
+	}
+}
+
+func TestBool(t *testing.T) {
+	if Bool("QWERTYQWERTY") {
+		t.Fail()
+	}
+	os.Setenv("QWERTYQWERTY", "yes")
+	if !Bool("QWERTYQWERTY") {
+		t.Fail()
+	}
+}
